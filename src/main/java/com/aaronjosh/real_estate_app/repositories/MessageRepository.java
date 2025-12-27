30f2d484acf3542356ba25484b12b323680f053a
shironzi <157195223+shironzi@users.noreply.github.com>
2025-12-27 20:51:12 +0800

    feat: remove unused sendMessage method from MessageRepository

--- src/main/java/com/aaronjosh/real_estate_app/repositories/MessageRepository.java
+++ src/main/java/com/aaronjosh/real_estate_app/repositories/MessageRepository.java
@@ -15,7 +15,4 @@ public interface MessageRepository extends JpaRepository<MessageEntity, UUID> {
     @Query("SELECT m FROM MessageEntity m " +
             "WHERE m.conversation.id = :conversationId")
     List<MessageEntity> findAllMessagesByConversationId(@Param("conversationId") UUID conversationId);
-
-    @Query("INSERT INTO MessageEntity () VALUES")
-    public void sendMessage(@Param("conversationId") UUID conversationId);
 }
