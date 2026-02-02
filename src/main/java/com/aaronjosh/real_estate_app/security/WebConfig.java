f7e3dec6a7a91a2531ceb0d9e593cf7075fc8dc9
shironzi <aaronbaon1@gmail.com>
2026-02-02 21:47:04 +0800

    fix: Correct CORS allowedOrigins by removing trailing slash for proper configuration

--- src/main/java/com/aaronjosh/real_estate_app/security/WebConfig.java
+++ src/main/java/com/aaronjosh/real_estate_app/security/WebConfig.java
@@ -10,10 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-
 }
