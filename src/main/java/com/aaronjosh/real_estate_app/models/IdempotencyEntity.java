86715e4aef9c929df727397df53924173e018d4c
shironzi <aaronbaon1@gmail.com>
2026-02-23 22:35:02 +0800

    feat: add IdempotencyEntity and IdempotencyService

---
+++ src/main/java/com/aaronjosh/real_estate_app/models/IdempotencyEntity.java
@@ -0,0 +1,45 @@
+package com.aaronjosh.real_estate_app.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "idempotency")
+@Data
+public class IdempotencyEntity {
+
+    public enum IdempotencyStatus {
+        COMPLETED,
+        PENDING,
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String key;
+
+    @Column(columnDefinition = "TEXT")
+    private String response;
+
+    @Column(nullable = false)
+    private IdempotencyStatus status = IdempotencyStatus.PENDING;
+
+    @Column()
+    private LocalDateTime created_at;
+
+    @PrePersist
+    protected void onCreate() {
+        created_at = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/aaronjosh/real_estate_app/repositories/IdempotencyRepo.java
@@ -0,0 +1,14 @@
+package com.aaronjosh.real_estate_app.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aaronjosh.real_estate_app.models.IdempotencyEntity;
+
+@Repository
+public interface IdempotencyRepo extends JpaRepository<IdempotencyEntity, UUID> {
+    Optional<IdempotencyEntity> findByKey(String key);
+}
