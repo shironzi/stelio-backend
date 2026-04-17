32e18b46e0e416639ec2dfc95c8f824784658503
shironzi <157195223+shironzi@users.noreply.github.com>
2026-04-17 15:00:17 +0800

    feat: Added idempotency for payment intent request

--- src/main/java/com/aaronjosh/real_estate_app/controllers/PaymentsController.java
+++ src/main/java/com/aaronjosh/real_estate_app/controllers/PaymentsController.java
@@ -1,20 +1,21 @@
 package com.aaronjosh.real_estate_app.controllers;
 
-import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.aaronjosh.real_estate_app.services.IdempotencyService;
 import com.aaronjosh.real_estate_app.services.PaymentService;
 import com.stripe.exception.StripeException;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 @Controller
 @RequestMapping("/api/payments")
@@ -23,14 +24,13 @@ public class PaymentsController {
     @Autowired
     private PaymentService paymentService;
 
-    @PostMapping("/{bookingId}")
-    public ResponseEntity<?> generateStripePaymentIntent(@Valid @PathVariable UUID bookingId) throws StripeException {
-        String StripePaymentIntentId = paymentService.generateStripePaymentIntent(bookingId);
+    @Autowired
+    private IdempotencyService idemptService;
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(Map.of(
-                        "success", true,
-                        "message", "Payment Successfully received.",
-                        "paymentId", StripePaymentIntentId));
+    @PostMapping("/{bookingId}")
+    public ResponseEntity<?> generateStripePaymentIntent(@Valid @PathVariable UUID bookingId,
+            @RequestHeader("Idempotency-Key") @NotNull String idempotencyKey) throws StripeException {
+
+        return idemptService.handle(idempotencyKey, () -> paymentService.generateStripePaymentIntent(bookingId));
     }
 }
--- src/main/java/com/aaronjosh/real_estate_app/services/PaymentService.java
+++ src/main/java/com/aaronjosh/real_estate_app/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.aaronjosh.real_estate_app.services;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,28 +34,37 @@ public class PaymentService {
     private BookingRepo bookingRepo;
 
     @Transactional
-    public String generateStripePaymentIntent(UUID bookingId) throws StripeException {
-        BookingEntity booking = bookingRepo.findById(bookingId).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Booking not found"));
+    public Map<String, Object> generateStripePaymentIntent(UUID bookingId) {
+        try {
+            BookingEntity booking = bookingRepo.findById(bookingId).orElseThrow(
+                    () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Booking not found"));
 
-        if (booking.getPaymentStatus() == PaymentStatus.PAID) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Booking already paid");
+            if (booking.getPaymentStatus() == PaymentStatus.PAID) {
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "Booking already paid");
+            }
+
+            PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
+                    .setAmount(booking.getBalance()
+                            .multiply(new BigDecimal("100"))
+                            .setScale(0, RoundingMode.HALF_UP)
+                            .longValue())
+                    .setCurrency("php")
+                    .build();
+
+            PaymentIntent paymentIntent = PaymentIntent.create(params);
+
+            booking.setStripePaymentIntentId(paymentIntent.getId());
+            bookingRepo.save(booking);
+
+            return Map.of(
+                    "success", true,
+                    "message", "Successfully generated payment intent secret",
+                    "paymentIntent", paymentIntent.getClientSecret());
+        } catch (StripeException e) {
+            throw new RuntimeException("Payment processing failed", e);
+        } catch (Exception e) {
+            throw new RuntimeException("An unexpected error occurred during generating payment intent operation", e);
         }
-
-        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
-                .setAmount(booking.getBalance()
-                        .multiply(new BigDecimal("100"))
-                        .setScale(0, RoundingMode.HALF_UP)
-                        .longValue())
-                .setCurrency("php")
-                .build();
-
-        PaymentIntent paymentIntent = PaymentIntent.create(params);
-
-        booking.setStripePaymentIntentId(paymentIntent.getId());
-        bookingRepo.save(booking);
-
-        return paymentIntent.getClientSecret();
     }
 
     @Transactional
