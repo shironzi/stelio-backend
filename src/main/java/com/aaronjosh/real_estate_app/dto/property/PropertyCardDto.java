65c894075b34a20c6ec405afb683548ca748d7b4
shironzi <aaronbaon1@gmail.com>
2026-04-30 07:40:02 +0800

    refactor: simplify image URL assignment in PropertyCardDto constructor

--- src/main/java/com/aaronjosh/real_estate_app/dto/property/PropertyCardDto.java
+++ src/main/java/com/aaronjosh/real_estate_app/dto/property/PropertyCardDto.java
@@ -2,11 +2,11 @@ package com.aaronjosh.real_estate_app.dto.property;
 
 import java.math.BigDecimal;
 import java.util.UUID;
+
 import lombok.Data;
 
 @Data
 public class PropertyCardDto {
-
     private UUID id;
     private String title;
     private String address;
@@ -18,11 +18,6 @@ public class PropertyCardDto {
         this.title = title;
         this.address = address;
         this.price = price;
-        this.imageUrl = buildUrl(imageKey);
-    }
-
-    private static String buildUrl(String imageKey) {
-        String publicUrl = System.getenv("CLOUDFLARE_R2_PUBLIC_URL");
-        return imageKey != null ? publicUrl + "/" + imageKey : null;
+        this.imageUrl = imageKey;
     }
 }
